97528bdf3c9f1d3f9843c7470d5dcf6e360910e3
William Zheng <williamzheng2001@gmail.com>
2024-07-26 19:49:58 -0400

    add treesitter textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,37 +1,114 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
-	cmd = { "TSUpdate" },
-	keys = {
-		{ "<c-space>" },
-		{ "<bs>" },
-	},
-	opts = {
-		highlight = { enable = true },
-		indent = { enable = true },
-		ensure_installed = {
-			"go",
-			"gomod",
-			"gosum",
-			"gotmpl",
-			"gowork",
-			"lua",
-			"nix",
-			"vim",
-			"vimdoc",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<c-space>",
-				node_incremental = "<c-space>",
-				scope_incremental = false,
-				node_decremental = "<bs>",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
+		cmd = { "TSUpdate" },
+		keys = {
+			{ "<C-space>" },
+			{ "<bs>" },
+		},
+		opts = {
+			highlight = { enable = true },
+			indent = { enable = true },
+			ensure_installed = {
+				"go",
+				"gomod",
+				"gosum",
+				"gotmpl",
+				"gowork",
+				"lua",
+				"nix",
+				"vim",
+				"vimdoc",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["i="] = "@assignment.inner",
+						["a="] = "@assignment.outer",
+						["l="] = "@assignment.lhs",
+						["r="] = "@assignment.rhs",
+						["im"] = "@call.inner",
+						["am"] = "@call.outer",
+						["ic"] = "@class.inner",
+						["ac"] = "@class.outer",
+						["ii"] = "@conditional.inner",
+						["ai"] = "@conditional.outer",
+						["if"] = "@function.inner",
+						["af"] = "@function.outer",
+						["il"] = "@loop.inner",
+						["al"] = "@loop.outer",
+						["ia"] = "@parameter.inner",
+						["aa"] = "@parameter.outer",
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>a"] = "@parameter.inner",
+					},
+					swap_previous = {
+						["<leader>A"] = "@parameter.inner",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]f"] = "@function.outer",
+						["]c"] = "@class.outer",
+						["]a"] = "@parameter.inner",
+					},
+					goto_next_end = {
+						["]F"] = "@function.outer",
+						["]C"] = "@class.outer",
+						["]A"] = "@parameter.inner",
+					},
+					goto_previous_start = {
+						["[f"] = "@function.outer",
+						["[c"] = "@class.outer",
+						["[a"] = "@parameter.inner",
+					},
+					goto_previous_end = {
+						["[F"] = "@function.outer",
+						["[C"] = "@class.outer",
+						["[A"] = "@parameter.inner",
+					},
+				},
 			},
 		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+
+			local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+			vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+			vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+
+			vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+		end,
 	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		lazy = true,
+	},
 }
