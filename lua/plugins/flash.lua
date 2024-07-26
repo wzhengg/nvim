147359f5abcefecca276ce11cfc1b1bbb520e51f
William Zheng <williamzheng2001@gmail.com>
2024-07-26 16:29:49 -0400

    remove flash.nvim

--- lua/plugins/flash.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  config = true,
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end },
-  }
-}
