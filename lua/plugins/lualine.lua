6fbf180a2b3bbd5780d2b5d55fb14dbaca0ea917
William Zheng <williamzheng2001@gmail.com>
2024-07-26 17:50:37 -0400

    change filename modified symbol

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,21 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = "VeryLazy",
+	event = "VeryLazy",
 	opts = {
 		options = {
 			section_separators = "",
 			component_separators = "",
+		},
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					symbols = {
+						modified = "●",
+					},
+				},
+			},
 		},
 	},
 }
