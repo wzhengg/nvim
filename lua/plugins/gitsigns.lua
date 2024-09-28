4284975bfe53b7d73e6232bea39b3ef0e7255d8d
William Zheng <williamzheng2001@gmail.com>
2024-09-27 22:54:04 -0400

    customize signs

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,5 +1,22 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-  config = true,
+	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+	opts = {
+		signs = {
+			add = { text = "+" },
+			change = { text = "~" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+		signs_staged = {
+			add = { text = "+" },
+			change = { text = "~" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+	},
 }
