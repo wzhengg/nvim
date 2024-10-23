f5875f22fa5d5b65e64f7ddeb10e3c254029d55f
William Zheng <williamzheng2001@gmail.com>
2024-10-22 21:20:17 -0400

    replace cmp with blink.nvim

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,65 +1,15 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	lazy = false,
+	version = "v0.*",
+	opts = {
+		highlight = {
+			use_nvim_cmp_as_default = true,
+		},
+		nerd_font_variant = "normal",
+		keymap = {
+			accept = "<cr>",
+		},
 	},
-	opts = function()
-		local cmp = require("cmp")
-
-		local icons = {
-			Text = "󰉿",
-			Method = "󰆧",
-			Function = "󰊕",
-			Constructor = "",
-			Field = "󰜢",
-			Variable = "󰀫",
-			Class = "󰠱",
-			Interface = "",
-			Module = "",
-			Property = "󰜢",
-			Unit = "󰑭",
-			Value = "󰎠",
-			Enum = "",
-			Keyword = "󰌋",
-			Snippet = "",
-			Color = "󰏘",
-			File = "󰈙",
-			Reference = "󰈇",
-			Folder = "󰉋",
-			EnumMember = "",
-			Constant = "󰏿",
-			Struct = "󰙅",
-			Event = "",
-			Operator = "󰆕",
-			TypeParameter = "󰅲",
-		}
-
-		return {
-			mapping = cmp.mapping.preset.insert({
-				["<c-b>"] = cmp.mapping.scroll_docs(-4),
-				["<c-f>"] = cmp.mapping.scroll_docs(4),
-				["<c-space>"] = cmp.mapping.complete(),
-				["<c-e>"] = cmp.mapping.abort(),
-				["<cr>"] = cmp.mapping.confirm({ select = true }),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "path" },
-			}, {
-				name = "buffer",
-			}),
-			formatting = {
-				format = function(_, item)
-					if icons[item.kind] then
-						item.kind = string.format("%s  %s", icons[item.kind], item.kind)
-					end
-
-					return item
-				end,
-			},
-		}
-	end,
 }
