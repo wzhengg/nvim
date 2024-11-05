aad86584d94410347a2d880802754906b5259e60
William Zheng <williamzheng2001@gmail.com>
2024-11-05 14:55:58 -0500

    enable blink.cmp integration

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,13 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
-	config = function()
+	opts = {
+		integrations = {
+			blink_cmp = true,
+		},
+	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
