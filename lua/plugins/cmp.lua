3f9597427a6147bead71adf51ac6c927050dd335
William Zheng <williamzheng2001@gmail.com>
2024-11-10 21:18:10 -0500

    use default keymap

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,21 +3,7 @@ return {
 	lazy = false,
 	version = "v0.*",
 	opts = {
-		keymap = {
-			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-			["<C-e>"] = { "hide" },
-			["<C-y>"] = { "select_and_accept" },
-			["<CR>"] = { "accept", "fallback" },
-
-			["<C-p>"] = { "select_prev", "fallback" },
-			["<C-n>"] = { "select_next", "fallback" },
-
-			["<C-b>"] = { "scroll_documentation_up", "fallback" },
-			["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-			["<Tab>"] = { "snippet_forward", "fallback" },
-			["<S-Tab>"] = { "snippet_backward", "fallback" },
-		},
+		keymap = { preset = "default" },
 
 		accept = {
 			create_undo_point = true,
