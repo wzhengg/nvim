832347c30c91f072169cc7f88c43f6d89faa4811
William Zheng <williamzheng2001@gmail.com>
2024-11-10 21:21:22 -0500

    highlight references

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,14 +32,40 @@ return {
 		end
 
 		vim.api.nvim_create_autocmd("LspAttach", {
-			callback = function(args)
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = args.buf })
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = args.buf })
-				vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { buffer = args.buf })
-				vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = args.buf })
-				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = args.buf })
-				vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = args.buf })
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
+			group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+
+			callback = function(event)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf })
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = event.buf })
+				vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { buffer = event.buf })
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = event.buf })
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = event.buf })
+				vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = event.buf })
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = event.buf })
+
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+					local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.document_highlight,
+					})
+
+					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.clear_references,
+					})
+
+					vim.api.nvim_create_autocmd("LspDetach", {
+						group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+						callback = function(event2)
+							vim.lsp.buf.clear_references()
+							vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
+						end,
+					})
+				end
 			end,
 		})
 	end,
