b5c240889f0e724707d5c0cd454870515d13934d
William Zheng <williamzheng2001@gmail.com>
2024-12-11 09:30:35 -0500

    use vim.uv

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,7 +14,7 @@ return {
 				enable = true,
 				disable = function(_, buf)
 					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
 					return ok and stats and stats.size > max_filesize
 				end,
 				additional_vim_regex_highlighting = false,
