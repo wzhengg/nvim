6f5b4f127bc66c1107abf5a13ef0d4e08271a792
William Zheng <williamzheng2001@gmail.com>
2025-01-22 21:55:26 -0500

    add eslint_d

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -5,13 +5,14 @@ return {
 		local lint = require("lint")
 
 		lint.linters_by_ft = {
-			-- javascript = { "eslint_d" },
-			-- typescript = { "eslint_d" },
-			-- javascriptreact = { "eslint_d" },
-			-- typescriptreact = { "eslint_d" },
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
 		}
 
 		vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+			group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
 			callback = function()
 				lint.try_lint()
 			end,
