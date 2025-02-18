a675c401d34b40e9609ca34ee353e41ce3c7e0cf
William Zheng <williamzheng2001@gmail.com>
2025-02-18 10:03:28 -0500

    use kanagawa theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,36 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	opts = {
-		integrations = {
-			blink_cmp = true,
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			integrations = {
+				blink_cmp = true,
+			},
 		},
+		config = function(_, opts)
+			require("catppuccin").setup(opts)
+			vim.cmd.colorscheme("catppuccin")
+		end,
+		enabled = false,
 	},
-	config = function(_, opts)
-		require("catppuccin").setup(opts)
-		vim.cmd.colorscheme("catppuccin")
-	end,
+
+	{
+		"rebelot/kanagawa.nvim",
+		opts = {
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			require("kanagawa").setup(opts)
+			vim.cmd.colorscheme("kanagawa-dragon")
+		end,
+	},
 }
