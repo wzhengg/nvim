6b229c353ae3fa9260d870540564fc4fb976ced2
William Zheng <williamzheng2001@gmail.com>
2025-02-26 10:46:18 -0500

    update blink.cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,15 +1,34 @@
 return {
 	"saghen/blink.cmp",
-	lazy = false,
+	-- lazy = false,
 	version = "*",
+
+	--@module 'blink.cmp'
+	--@type blink.cmp.Config
 	opts = {
-		keymap = { preset = "default" },
+		keymap = {
+			preset = "none",
+
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide" },
+			["<C-y>"] = { "select_and_accept" },
+
+			["<C-p>"] = { "select_prev", "fallback" },
+			["<C-n>"] = { "select_next", "fallback" },
+
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+			["<Tab>"] = { "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+		},
 
 		appearance = { nerd_font_variant = "normal" },
 
 		sources = {
 			default = { "lsp", "path" },
-			cmdline = {},
 		},
 
 		completion = {
@@ -17,19 +36,16 @@ return {
 				create_undo_point = true,
 				auto_brackets = { enabled = false },
 			},
-			documentation = {
-				window = { border = "single" },
-			},
+			documentation = {},
 			ghost_text = { enabled = true },
+			keyword = { range = "full" },
 			list = {
 				selection = { preselect = false, auto_insert = false },
 			},
-			menu = { border = "single" },
 		},
 
 		signature = {
 			enabled = true,
-			window = { border = "single" },
 		},
 	},
 	opts_extend = { "sources.default" },
