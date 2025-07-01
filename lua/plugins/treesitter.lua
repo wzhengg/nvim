e06cb96dde0a2b1df35b8209b0ba57084041c0ed
William Zheng <williamzheng2001@gmail.com>
2025-07-01 12:55:37 -0400

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,103 +1,53 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
-		build = ":TSUpdate",
-		event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
-		cmd = { "TSUpdate" },
-		keys = {
-			{ "<C-space>" },
-			{ "<BS>" },
-		},
-		opts = {
-			highlight = {
-				enable = true,
-				disable = function(_, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-					return ok and stats and stats.size > max_filesize
-				end,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-			ensure_installed = {
-				"dockerfile",
-				"html",
-				"json",
-				"markdown",
-				"markdown_inline",
-				"nix",
-				"sql",
-				"vim",
-				"vimdoc",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<BS>",
-				},
-			},
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["a="] = "@assignment.lhs",
-						["i="] = "@assignment.rhs",
-						["im"] = "@call.inner",
-						["am"] = "@call.outer",
-						["ic"] = "@class.inner",
-						["ac"] = "@class.outer",
-						["ii"] = "@conditional.inner",
-						["ai"] = "@conditional.outer",
-						["if"] = "@function.inner",
-						["af"] = "@function.outer",
-						["il"] = "@loop.inner",
-						["al"] = "@loop.outer",
-						["ia"] = "@parameter.inner",
-						["aa"] = "@parameter.outer",
-					},
-				},
-				swap = {
-					enable = true,
-					swap_next = {
-						["<leader>a"] = "@parameter.inner",
-					},
-					swap_previous = {
-						["<leader>A"] = "@parameter.inner",
-					},
-				},
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]f"] = "@function.outer",
-						["]c"] = "@class.outer",
-						["]a"] = "@parameter.inner",
-					},
-					goto_next_end = {
-						["]F"] = "@function.outer",
-						["]C"] = "@class.outer",
-						["]A"] = "@parameter.inner",
-					},
-					goto_previous_start = {
-						["[f"] = "@function.outer",
-						["[c"] = "@class.outer",
-						["[a"] = "@parameter.inner",
-					},
-					goto_previous_end = {
-						["[F"] = "@function.outer",
-						["[C"] = "@class.outer",
-						["[A"] = "@parameter.inner",
-					},
-				},
-			},
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-	},
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+	branch = "main",
+	build = ":TSUpdate",
+	lazy = false,
+	config = function()
+		require("nvim-treesitter").install({
+			"dockerfile",
+			"html",
+			"json",
+			"markdown",
+			"markdown_inline",
+			"nix",
+			"sql",
+			"vim",
+			"vimdoc",
+
+			"go",
+			"gomod",
+			"gosum",
+			"gotmpl",
+			"gowork",
+
+			"lua",
+
+			"javascript",
+			"typescript",
+			"tsx",
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			-- pattern = opts.languages,
+			callback = function(args)
+				local buf = args.buf
+
+				local max_filesize = 100 * 1024 -- 100 KB
+				local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+
+				if ok and stats and stats.size > max_filesize then
+					return
+				end
+
+				if not pcall(vim.treesitter.start, buf) then
+					return
+				end
+
+				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- folds
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation
+			end,
+		})
+	end,
 }
