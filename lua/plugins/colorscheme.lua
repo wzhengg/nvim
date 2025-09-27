10b8d577419bb8d2d05c57882fc928126c0b019c
William Zheng <williamzheng2001@gmail.com>
2025-09-26 21:28:24 -0400

    Revert "use kanagawa theme"
    This reverts commit 364f814d038b174f93a65628ce64bdaa4a24cd50.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,37 +1,14 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		enabled = false,
-		priority = 1000,
-		opts = {
-			integrations = {
-				blink_cmp = true,
-			},
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		integrations = {
+			blink_cmp = true,
 		},
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-			vim.cmd.colorscheme("catppuccin")
-		end,
 	},
-
-	{
-		"rebelot/kanagawa.nvim",
-		priority = 1000,
-		opts = {
-			colors = {
-				theme = {
-					all = {
-						ui = {
-							bg_gutter = "none",
-						},
-					},
-				},
-			},
-		},
-		config = function(_, opts)
-			require("kanagawa").setup(opts)
-			vim.cmd.colorscheme("kanagawa")
-		end,
-	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
