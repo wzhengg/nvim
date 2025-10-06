1d2d895a89247346d046ba7a9cd8832bdf90fb51
William Zheng <williamzheng2001@gmail.com>
2025-10-05 22:28:18 -0400

    add treesitter-textobjects

--- init.lua
+++ init.lua
@@ -45,9 +45,10 @@ vim.pack.add({
 	{ src = "https://github.com/ibhagwan/fzf-lua" },
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/nvim-mini/mini.icons",            version = "main", },
-	{ src = "https://github.com/nvim-mini/mini.statusline",       version = "main", },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main", },
+	{ src = "https://github.com/nvim-mini/mini.icons",                        version = "main", },
+	{ src = "https://github.com/nvim-mini/mini.statusline",                   version = "main", },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter",             version = "main", },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects", version = "main", },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 })
 
@@ -195,6 +196,95 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 	end
 })
+
+-- treesitter-textobjects
+
+require("nvim-treesitter-textobjects").setup({
+	select = {
+		lookahead = true,
+		selection_modes = {
+			["@assignment.lhs"] = "v",
+			["@assignment.rhs"] = "v",
+			["@assignment.outer"] = "v",
+			["@call.inner"] = "v",
+			["@call.outer"] = "v",
+			["@class.inner"] = "v",
+			["@class.outer"] = "v",
+			["@conditional.inner"] = "v",
+			["@conditional.outer"] = "v",
+			["@function.inner"] = "v",
+			["@function.outer"] = "v",
+			["@loop.inner"] = "v",
+			["@loop.outer"] = "v",
+			["@parameter.inner"] = "v",
+			["@parameter.outer"] = "v",
+			["@statement.outer"] = "v",
+		},
+		include_surrounding_whitespace = false,
+	},
+	move = {
+		set_jumps = true,
+	},
+})
+
+local ts_select_mappings = {
+	{ mode = { "x", "o" }, lhs = "a=", query_string = "@assignment.lhs" },
+	{ mode = { "x", "o" }, lhs = "i=", query_string = "@assignment.rhs" },
+	{ mode = { "x", "o" }, lhs = "a:", query_string = "@assignment.outer" },
+	{ mode = { "x", "o" }, lhs = "im", query_string = "@call.inner" },
+	{ mode = { "x", "o" }, lhs = "am", query_string = "@call.outer" },
+	{ mode = { "x", "o" }, lhs = "ic", query_string = "@class.inner" },
+	{ mode = { "x", "o" }, lhs = "ac", query_string = "@class.outer" },
+	{ mode = { "x", "o" }, lhs = "ii", query_string = "@conditional.inner" },
+	{ mode = { "x", "o" }, lhs = "ai", query_string = "@conditional.outer" },
+	{ mode = { "x", "o" }, lhs = "if", query_string = "@function.inner" },
+	{ mode = { "x", "o" }, lhs = "af", query_string = "@function.outer" },
+	{ mode = { "x", "o" }, lhs = "il", query_string = "@loop.inner" },
+	{ mode = { "x", "o" }, lhs = "al", query_string = "@loop.outer" },
+	{ mode = { "x", "o" }, lhs = "ia", query_string = "@parameter.inner" },
+	{ mode = { "x", "o" }, lhs = "aa", query_string = "@parameter.outer" },
+	{ mode = { "x", "o" }, lhs = "a;", query_string = "@statement.outer" },
+}
+
+for _, mapping in ipairs(ts_select_mappings) do
+	vim.keymap.set(mapping.mode, mapping.lhs, function()
+		require("nvim-treesitter-textobjects.select").select_textobject(mapping.query_string, "textobjects")
+	end)
+end
+
+local ts_swap_mappings = {
+	{ mode = { "n" }, lhs = "<leader>a", swap_fn = require("nvim-treesitter-textobjects.swap").swap_next,     query_string = "@parameter.inner" },
+	{ mode = { "n" }, lhs = "<leader>A", swap_fn = require("nvim-treesitter-textobjects.swap").swap_previous, query_string = "@parameter.inner" },
+	{ mode = { "n" }, lhs = "<leader>}", swap_fn = require("nvim-treesitter-textobjects.swap").swap_next,     query_string = "@function.outer" },
+	{ mode = { "n" }, lhs = "<leader>{", swap_fn = require("nvim-treesitter-textobjects.swap").swap_previous, query_string = "@function.outer" },
+}
+
+for _, mapping in ipairs(ts_swap_mappings) do
+	vim.keymap.set(mapping.mode, mapping.lhs, function()
+		mapping.swap_fn(mapping.query_string)
+	end)
+end
+
+local ts_move_mappings = {
+	{ mode = { "n", "x", "o" }, lhs = "]f", goto_fn = require("nvim-treesitter-textobjects.move").goto_next_start,     query_string = "@function.outer" },
+	{ mode = { "n", "x", "o" }, lhs = "[f", goto_fn = require("nvim-treesitter-textobjects.move").goto_previous_start, query_string = "@function.outer" },
+	{ mode = { "n", "x", "o" }, lhs = "]c", goto_fn = require("nvim-treesitter-textobjects.move").goto_next_start,     query_string = "@class.outer" },
+	{ mode = { "n", "x", "o" }, lhs = "[c", goto_fn = require("nvim-treesitter-textobjects.move").goto_previous_start, query_string = "@class.outer" },
+	{ mode = { "n", "x", "o" }, lhs = "]a", goto_fn = require("nvim-treesitter-textobjects.move").goto_next_start,     query_string = "@parameter.inner" },
+	{ mode = { "n", "x", "o" }, lhs = "[a", goto_fn = require("nvim-treesitter-textobjects.move").goto_previous_start, query_string = "@parameter.inner" },
+	{ mode = { "n", "x", "o" }, lhs = "]F", goto_fn = require("nvim-treesitter-textobjects.move").goto_next_end,       query_string = "@function.outer" },
+	{ mode = { "n", "x", "o" }, lhs = "[F", goto_fn = require("nvim-treesitter-textobjects.move").goto_previous_end,   query_string = "@function.outer" },
+	{ mode = { "n", "x", "o" }, lhs = "]C", goto_fn = require("nvim-treesitter-textobjects.move").goto_next_end,       query_string = "@class.outer" },
+	{ mode = { "n", "x", "o" }, lhs = "[C", goto_fn = require("nvim-treesitter-textobjects.move").goto_previous_end,   query_string = "@class.outer" },
+	{ mode = { "n", "x", "o" }, lhs = "]A", goto_fn = require("nvim-treesitter-textobjects.move").goto_next_end,       query_string = "@parameter.inner" },
+	{ mode = { "n", "x", "o" }, lhs = "[A", goto_fn = require("nvim-treesitter-textobjects.move").goto_previous_end,   query_string = "@parameter.inner" },
+}
+
+for _, mapping in ipairs(ts_move_mappings) do
+	vim.keymap.set(mapping.mode, mapping.lhs, function()
+		mapping.goto_fn(mapping.query_string, "textobjects")
+	end)
+end
 
 -- oil
 
