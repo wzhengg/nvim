e469c5f83e699914ea9332ee2a9383ff8c3f0e11
William Zheng <williamzheng2001@gmail.com>
2026-01-25 17:01:02 -0500

    remove treesitter textobjects

--- init.lua
+++ init.lua
@@ -38,10 +38,9 @@ vim.pack.add({
 	{ src = "https://github.com/ibhagwan/fzf-lua" },
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/nvim-mini/mini.icons",                        version = "main", },
-	{ src = "https://github.com/nvim-mini/mini.statusline",                   version = "main", },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter",             version = "main", },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects", version = "main", },
+	{ src = "https://github.com/nvim-mini/mini.icons",            version = "main", },
+	{ src = "https://github.com/nvim-mini/mini.statusline",       version = "main", },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main", },
 	{ src = "https://github.com/rebelot/kanagawa.nvim" },
 	{ src = "https://github.com/stevearc/conform.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
@@ -216,100 +215,10 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
--- treesitter-textobjects
-
-require("nvim-treesitter-textobjects").setup({
-	select = {
-		lookahead = true,
-		selection_modes = {
-			["@assignment.lhs"] = "v",
-			["@assignment.rhs"] = "v",
-			["@assignment.outer"] = "v",
-			["@call.inner"] = "v",
-			["@call.outer"] = "v",
-			["@class.inner"] = "v",
-			["@class.outer"] = "v",
-			["@conditional.inner"] = "v",
-			["@conditional.outer"] = "v",
-			["@function.inner"] = "v",
-			["@function.outer"] = "v",
-			["@loop.inner"] = "v",
-			["@loop.outer"] = "v",
-			["@parameter.inner"] = "v",
-			["@parameter.outer"] = "v",
-			["@statement.outer"] = "v",
-		},
-		include_surrounding_whitespace = false,
-	},
-	move = {
-		set_jumps = true,
-	},
-})
-
-local ts_select_mappings = {
-	{ mode = { "x", "o" }, lhs = "a=", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@assignment.lhs" },
-	{ mode = { "x", "o" }, lhs = "i=", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@assignment.rhs" },
-	{ mode = { "x", "o" }, lhs = "a:", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@assignment.outer" },
-	{ mode = { "x", "o" }, lhs = "im", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@call.inner" },
-	{ mode = { "x", "o" }, lhs = "am", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@call.outer" },
-	{ mode = { "x", "o" }, lhs = "ic", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@class.inner" },
-	{ mode = { "x", "o" }, lhs = "ac", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@class.outer" },
-	{ mode = { "x", "o" }, lhs = "ii", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@conditional.inner" },
-	{ mode = { "x", "o" }, lhs = "ai", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@conditional.outer" },
-	{ mode = { "x", "o" }, lhs = "if", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@function.inner" },
-	{ mode = { "x", "o" }, lhs = "af", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@function.outer" },
-	{ mode = { "x", "o" }, lhs = "il", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@loop.inner" },
-	{ mode = { "x", "o" }, lhs = "al", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@loop.outer" },
-	{ mode = { "x", "o" }, lhs = "ia", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@parameter.inner" },
-	{ mode = { "x", "o" }, lhs = "aa", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@parameter.outer" },
-	{ mode = { "x", "o" }, lhs = "a;", select_fn = require("nvim-treesitter-textobjects.select").select_textobject, query_string = "@statement.outer" },
-}
-
-for _, mapping in ipairs(ts_select_mappings) do
-	vim.keymap.set(mapping.mode, mapping.lhs, function()
-		require("nvim-treesitter-textobjects.select").select_textobject(mapping.query_string, "textobjects")
-	end)
-end
-
-local ts_swap_mappings = {
-	{ mode = { "n" }, lhs = "<leader>a", swap_fn = require("nvim-treesitter-textobjects.swap").swap_next,     query_string = "@parameter.inner" },
-	{ mode = { "n" }, lhs = "<leader>A", swap_fn = require("nvim-treesitter-textobjects.swap").swap_previous, query_string = "@parameter.inner" },
-	{ mode = { "n" }, lhs = "<leader>}", swap_fn = require("nvim-treesitter-textobjects.swap").swap_next,     query_string = "@function.outer" },
-	{ mode = { "n" }, lhs = "<leader>{", swap_fn = require("nvim-treesitter-textobjects.swap").swap_previous, query_string = "@function.outer" },
-}
-
-for _, mapping in ipairs(ts_swap_mappings) do
-	vim.keymap.set(mapping.mode, mapping.lhs, function()
-		mapping.swap_fn(mapping.query_string)
-	end)
-end
-
-local ts_move_mappings = {
-	{ mode = { "n", "x", "o" }, lhs = "]f", goto_fn = require("nvim-treesitter-textobjects.move").goto_next_start,     query_string = "@function.outer" },
-	{ mode = { "n", "x", "o" }, lhs = "[f", goto_fn = require("nvim-treesitter-textobjects.move").goto_previous_start, query_string = "@function.outer" },
-	{ mode = { "n", "x", "o" }, lhs = "]c", goto_fn = require("nvim-treesitter-textobjects.move").goto_next_start,     query_string = "@class.outer" },
-	{ mode = { "n", "x", "o" }, lhs = "[c", goto_fn = require("nvim-treesitter-textobjects.move").goto_previous_start, query_string = "@class.outer" },
-	{ mode = { "n", "x", "o" }, lhs = "]a", goto_fn = require("nvim-treesitter-textobjects.move").goto_next_start,     query_string = "@parameter.inner" },
-	{ mode = { "n", "x", "o" }, lhs = "[a", goto_fn = require("nvim-treesitter-textobjects.move").goto_previous_start, query_string = "@parameter.inner" },
-	{ mode = { "n", "x", "o" }, lhs = "]F", goto_fn = require("nvim-treesitter-textobjects.move").goto_next_end,       query_string = "@function.outer" },
-	{ mode = { "n", "x", "o" }, lhs = "[F", goto_fn = require("nvim-treesitter-textobjects.move").goto_previous_end,   query_string = "@function.outer" },
-	{ mode = { "n", "x", "o" }, lhs = "]C", goto_fn = require("nvim-treesitter-textobjects.move").goto_next_end,       query_string = "@class.outer" },
-	{ mode = { "n", "x", "o" }, lhs = "[C", goto_fn = require("nvim-treesitter-textobjects.move").goto_previous_end,   query_string = "@class.outer" },
-	{ mode = { "n", "x", "o" }, lhs = "]A", goto_fn = require("nvim-treesitter-textobjects.move").goto_next_end,       query_string = "@parameter.inner" },
-	{ mode = { "n", "x", "o" }, lhs = "[A", goto_fn = require("nvim-treesitter-textobjects.move").goto_previous_end,   query_string = "@parameter.inner" },
-}
-
-for _, mapping in ipairs(ts_move_mappings) do
-	vim.keymap.set(mapping.mode, mapping.lhs, function()
-		mapping.goto_fn(mapping.query_string, "textobjects")
-	end)
-end
-
 -- oil
 
 require("oil").setup({
 	default_file_explorer = true,
-	delete_to_trash = true,
 	skip_confirm_for_simple_edits = true,
 })
 
@@ -347,25 +256,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- clear search highlights
 vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR>")
-
--- keep selection on indent
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
-
--- jump to diagnostics
-local function diagnostic_jump(count, severity)
-	return function()
-		vim.diagnostic.jump({ count = count, severity = severity })
-	end
-end
-vim.keymap.set("n", "[e", diagnostic_jump(-1, vim.diagnostic.severity.ERROR))
-vim.keymap.set("n", "]e", diagnostic_jump(1, vim.diagnostic.severity.ERROR))
-vim.keymap.set("n", "[w", diagnostic_jump(-1, vim.diagnostic.severity.WARN))
-vim.keymap.set("n", "]w", diagnostic_jump(1, vim.diagnostic.severity.WARN))
-vim.keymap.set("n", "[i", diagnostic_jump(-1, vim.diagnostic.severity.INFO))
-vim.keymap.set("n", "]i", diagnostic_jump(1, vim.diagnostic.severity.INFO))
-vim.keymap.set("n", "[h", diagnostic_jump(-1, vim.diagnostic.severity.HINT))
-vim.keymap.set("n", "]h", diagnostic_jump(1, vim.diagnostic.severity.HINT))
 
 -- quality-of-life yank, delete, paste
 vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y')
